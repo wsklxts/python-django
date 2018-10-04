612e343e5677eb1baa6dcb0d2b3a21a58a372d77
wsklxts <276662340@qq.com>
2018-10-04 14:32:36 +0800

    defualt

---
+++ dj/utils/mysql.py
@@ -0,0 +1,29 @@
+import pymysql
+
+def con():
+    return  pymysql.connect(host='localhost',
+                           user='root',
+                           password='root',
+                           db='ll',
+                           charset='utf8')
+
+def change(sql,arg):
+    conn = con()
+    cursor = conn.cursor()
+    try:
+        cursor.execute(sql,arg)
+        conn.commit()  # 提交，不然无法保存插入或者修改的数据(这个一定不要忘记加上)
+    except Exception as e:
+        return False
+    cursor.close()  # 关闭游标
+    conn.close()  # 关闭连接
+    return True
+
+def view(sql, arg):
+    conn = con()
+    cursor = conn.cursor(cursor=pymysql.cursors.DictCursor)
+    cursor.execute(sql, arg)
+    result = cursor.fetchall()
+    cursor.close()  # 关闭游标
+    conn.close()  # 关闭连接
+    return result
